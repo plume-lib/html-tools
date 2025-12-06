5ad9299b1d4a2480924b05598f3de00516ec1fd4
Michael Ernst <mernst@cs.washington.edu>
2025-12-05 17:18:28 -0800

    Add dummy Python file

---
+++ dummy.py
@@ -0,0 +1,7 @@
+"""Dummy file to avoid `ty` failure.
+
+`ty` fails if called only on files without the `.py` extension.
+(And, it doesn't do anything with them even if they are explicitly passed on the command line!)
+This file exists to avoid that error, since (at the time of writing)
+all other Python files in the directory lack the `.py` extension.
+"""
